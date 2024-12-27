3fd8f7107860b0bf7584e29369e1526e9c867ce5
mburuxx <njihiamburu.3@gmail.com>
2024-12-27 15:00:00 +0300

    Added an outlet

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,57 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
 } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+}
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is the home page</div>
+    element: <Layout/>,
+    children: [
+      {
+        path: "/home",
+        element: <Home/>
+      },
+
+      {
+        path: "/post/:id",
+        element: <Single/>
+      },
+    
+      {
+        path: "/write",
+        element: <Write/>
+      },
+    ]
   },
+
+  {
+    path: "/register",
+    element: <Register/>
+  },
+
+  {
+    path: "/login",
+    element: <Login/>
+  },
+
 ]);
 
 function App() {
