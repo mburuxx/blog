17a0284fe48d0a8cbc921481547972ea29001309
mburuxx <njihiamburu.3@gmail.com>
2024-12-28 07:52:33 +0300

    Style the navbar and footer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Logo from "../img/logo.png"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className="container">
+            <div className="logo">
+                <img src={Logo} alt=""/>
+            </div>
+            <div className="links">
+                <Link className='link' to="/?cat=art">
+                <h6>Technology</h6>
+                </Link>
+
+                <Link className='link' to="/?cat=art">
+                <h6>Finance</h6>
+                </Link>
+
+                <Link className='link' to="/?cat=art">
+                <h6>Agriculture</h6>
+                </Link>
+
+                <Link className='link' to="/?cat=art">
+                <h6>Other</h6>
+                </Link>
+
+                <span>Jhn</span>
+                <span>Logout</span>
+                <span className="write">
+                    <Link className="link" to="/write">Write</Link>
+                </span>
+            </div>
+        </div>
+    </div>
   )
 }
 
