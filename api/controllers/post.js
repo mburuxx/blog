c7ca10a177ad2794d1a08eca8b02b64315a11f58
mburuxx <njihiamburu.3@gmail.com>
2025-01-06 14:45:41 +0300

    Verify delete post using jwt authorization

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken"
 
 export const getPosts = (req, res) => {
   const q = req.query.cat
@@ -13,23 +14,37 @@ export const getPosts = (req, res) => {
 };
 
 export const getPost = (req, res) => {
-    const q = "SELECT `username`, `title`, `desc`, p.img, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id=?"
+    const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?"
 
     db.query(q, [req.params.id], (err, data) => {
-      if (err) return res.json(err)
+      if (err) return res.status(500).json(err)
 
       return res.status(200).json(data[0]);
     });
-}
+};
 
 export const addPost = (req, res) => {
     res.json("From controller")
-}
+};
 
 export const deletePost = (req, res) => {
-    res.json("From controller")
-}
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated!");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+      if (err) return res.status(403).json("Invalid token!");
+
+      const postId = req.params.id;
+      const q = "DELETE FROM posts WHERE `id`=? AND `uid`=?";
+
+      db.query(q, [postId, userInfo.id], (err, data) => {
+        if (err) return res.status(403).json("You can delete only your post!");
+
+        return res.json("Post deleted!");
+      });
+    });
+};
 
 export const updatePost = (req, res) => {
     res.json("From controller")
-}
+};
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Edit from "../img/edit.png"
 import Delete from "../img/delete.png"
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Menu from '../components/Menu'
 import moment from "moment"
 import axios from 'axios'
@@ -13,9 +13,11 @@ const Single = () => {
 
   const location = useLocation();
 
-  const postId = location.pathname.split("/", [2]);
+  const navigate = useNavigate();
 
-  const {currentUser} = useContext(AuthContext);
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,27 +31,39 @@ const Single = () => {
     fetchData();
   }, [postId]);
 
+  const handleDelete = () => {
+    const fetchData = async () => {
+      try {
+        await axios.delete(`/posts/${postId}`);
+        navigate("/");
+      } catch(err) {
+        console.log(err)
+      }
+    };
+    fetchData();
+  }
+
   return (
     <div className='single'>
       <div className="content">
         <img src={post?.img} alt=""/>
         <div className="user">
-          <img src="https://images.pexels.com/photos/26052213/pexels-photo-26052213/free-photo-of-young-woman-with-an-umbrella.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+          {post.userImg && <img src={post.userImg} alt=""/>}
             <div className="info">
               <span>{post.username}</span>
               <p>Posted {moment(post.date).fromNow()}</p>
             </div>
-            {currentUser.username == post.username && <div className="edit">
+            {currentUser.username === post.username && (<div className="edit">
               <Link to={`/write?edit=`}><img src={Edit} alt=""/></Link>
-              <img src={Delete} alt=""/>
-            </div>}
+              <img onClick={handleDelete} src={Delete} alt=""/>
+            </div>)}
         </div> 
         <h1>{post.title}</h1> 
         {post.desc}
       </div>
       <Menu/>
     </div>
-  )
-}
+  );
+};
 
 export default Single
